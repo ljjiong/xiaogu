0f043c370e7ec213dd85e26d952fb3e887b51c5c
ljjiong <52107223+ljjiong@users.noreply.github.com>
2019-10-27 13:29:49 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+// [ 应用入口文件 ]
+
+// 测试环境
+defined('WANCLL_ENV') or define('WANCLL_ENV', 'dev');
+
+// 产品环境
+// defined('WANCLL_ENV') or define('WANCLL_ENV', 'prod');
+
+// API版本号
+defined('API_VERSION') or define('API_VERSION', '1.4.0');
+
+// 定义应用目录
+define('APP_PATH', __DIR__ . '/application/');
+// 加载框架引导文件
+require __DIR__ . '/thinkphp/start.php';
